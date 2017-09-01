4a8178854da9014af244f55d4e42d1630fe104f1
hezongzhu <hezongzhu@gmail.com>
2017-09-01 09:42:10 +0800

    Initial Commit

---
+++ MMUITextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMUITextView
+//
+//  Created by Pillar on 2017/9/1.
+//  Copyright © 2017年 unkown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MMUITextView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MMUITextView
+//
+//  Created by Pillar on 2017/9/1.
+//  Copyright © 2017年 unkown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
